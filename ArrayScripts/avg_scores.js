310f5ff5365a977c2d0ba871539fe8fe2f6d07dd
LeilaC1 <leilaclesca13@gmail.com>
2023-11-02 11:34:07 -0400

    added getMedian

--- ArrayScripts/avg_scores.js
+++ ArrayScripts/avg_scores.js
@@ -16,3 +16,22 @@ let myScores = [92, 98, 84, 76, 89, 99, 100];
 
  console.log("my average: " + myAvg);
  console.log("your average: " + yourAvg);
+ 
+ function getMedian(score){
+ let oddNum = score.length % 2 === 1;
+ if (oddNum) {
+    let median = score[Math.floor(myScores.length / 2)];
+    return median;
+    
+ } else {
+    let numOne = Math.floor((score.length - 1) / 2);
+    let numTwo = numOne + 1;
+    let avg = (score[numOne] + score[numTwo]) / 2;
+    return avg;
+    
+ }
+}
+let myMedian = getMedian(myScores);
+let yourMedian = getMedian(yourScores);
+console.log("my median: " + myMedian);
+ console.log("your median: " + yourMedian);
